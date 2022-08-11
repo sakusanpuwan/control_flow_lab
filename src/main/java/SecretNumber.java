254ecd53f570efd6aedfcafceaa64d728de19a7d
sakusanpuwan <sakusanpuwan98@gmail.com>
2022-08-11 19:06:30 +0100

    added secret number game

--- src/main/java/SecretNumber.java
+++ src/main/java/SecretNumber.java
@@ -1,7 +1,27 @@
+import java.util.Scanner;
+
 public class SecretNumber {
     public static void main(String[] args) {
-        int secretNumber
+        int secretNumber = 24;
+        System.out.println("What's my secret number?");
+        Scanner reader = new Scanner(System.in);
+        int guess = Integer.parseInt(reader.nextLine());
+
+        if (guess == secretNumber){
+            System.out.println("You guessed right!");
+        } else if (guess > secretNumber) {
+            System.out.println("Too high!");
+        } else {
+            System.out.println("Too low!");
+        }
+
+
+
+
+
+
     }
 
-    int secretNumber = 24
+
 }
+
